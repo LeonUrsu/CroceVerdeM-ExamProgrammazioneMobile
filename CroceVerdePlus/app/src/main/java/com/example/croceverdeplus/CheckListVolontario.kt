b7d68a7f7e9d0cefe6e5d353fbc950ad4b5764e4
leonursu <leonursu1998@gmail.com>
2023-05-25 15:09:07 +0200

    fragment checklist volontario

--- CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/CheckListVolontario.kt
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/CheckListVolontario.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TabelloneTurniDipendente.newInstance] factory method to
+ * Use the [CheckListVolontario.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TabelloneTurniDipendente : Fragment() {
+class CheckListVolontario : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class TabelloneTurniDipendente : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabellone_turni_dipendente, container, false)
+        return inflater.inflate(R.layout.fragment_check_list_volontario, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class TabelloneTurniDipendente : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TabelloneTurniDipendente.
+         * @return A new instance of fragment CheckListVolontario.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TabelloneTurniDipendente().apply {
+            CheckListVolontario().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
