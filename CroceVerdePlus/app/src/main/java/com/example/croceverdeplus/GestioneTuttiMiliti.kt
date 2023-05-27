88590a8eb24fecade0a0f9e1b689e20f91bbd4b4
leonursu <leonursu1998@gmail.com>
2023-05-27 22:13:50 +0200

    risolto problema gradle, sol: cancellata cache e pulito codice da floating button

--- CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/GestioneTuttiMiliti.kt
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/GestioneTuttiMiliti.kt
@@ -37,23 +37,6 @@ class GestioneTuttiMiliti : Fragment() {
         return inflater.inflate(R.layout.fragment_gestione_tutti_militi, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GestioneTuttiMiliti.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GestioneTuttiMiliti().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }
