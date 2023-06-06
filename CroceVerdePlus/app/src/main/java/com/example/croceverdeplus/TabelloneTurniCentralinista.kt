0b733504b8a658b7e1e8c2566f7ea3ea7625a4b6
leonursu <leonursu1998@gmail.com>
2023-06-06 11:42:17 +0200

    tabelle 118 e h24 modificate

--- CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/TabelloneTurniCentralinista.kt
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/TabelloneTurniCentralinista.kt
@@ -5,28 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Spinner
 import android.widget.ViewFlipper
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TabelloneTurniCentralinista.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TabelloneTurniCentralinista : Fragment() {
-    // TODO: Rename and change types of parameters
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //findViewById<View>(R.id.tabellone_turni_118).setVisibility(View.VISIBLE)
-        //findViewById<View>(R.id.tabellone_turni_h24_118).setVisibility(View.GONE)
     }
 
     override fun onCreateView(
@@ -36,15 +20,9 @@ class TabelloneTurniCentralinista : Fragment() {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_tabellone_turni_centralinista, container, false)
         val vf = root.findViewById(R.id.vf) as ViewFlipper
-        vf.setDisplayedChild(1); //TODO qui si cambia settimana h24/118 & 118 si passa il valore 1 o 2
-        val input_servizio = root.findViewById<Spinner>(R.id.servizio_input)
+        vf.setDisplayedChild(1); //TODO qui si cambia settimana h24/118 & 118 si passa il valore 1 o 2 una volta implementato il metodo di scelta in amministratore
+        //TODO si deve implementare il modo per far vedere la lista dei volontari disponibili ne tabellone turni
         return root
     }
-
-
-
-
-
-
 
 }
