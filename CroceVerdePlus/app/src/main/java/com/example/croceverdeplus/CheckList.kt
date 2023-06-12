b9e2d51307e24a17accf43c549339f6db3bd7c40
leonursu <leonursu1998@gmail.com>
2023-06-12 11:03:29 +0200

    tabella

--- CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/CheckList.kt
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/CheckList.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
