1eaaadb27998c8e479fdf90aa4ef6f4c4356056e
andreascalzi <s1100029@studenti.univpm.it>
2023-06-16 11:16:23 +0200

    Add User e Database

---
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/Database.kt
@@ -0,0 +1,48 @@
+package com.example.croceverdeplus
+
+import android.content.ContentValues
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class Database {
+
+    private val db = Firebase.firestore
+
+    fun addUser(username: String, password: String) {
+
+        val user = User(username, password)
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                //Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding document", e)
+            }
+
+    }
+
+    fun deleteUser(username: String, password: String) {
+
+        db.collection("users")
+            .whereEqualTo("username", username)
+            .whereEqualTo("password", password)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(ContentValues.TAG, "${document.data} => ${document.id}")
+                    db.collection("users").document(document.id)
+                        .delete()
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully deleted!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error deleting document", e) }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting documents: ", exception)
+            }
+
+    }
+}
---
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/User.kt
@@ -0,0 +1,5 @@
+package com.example.croceverdeplus
+
+data class User(val username: String? = null,
+                val password: String? = null) {
+}
