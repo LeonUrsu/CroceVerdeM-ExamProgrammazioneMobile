6ff5c98bdefae5501ac2e7ebcdab7fe9dc98fd1f
andreascalzi <s1100029@studenti.univpm.it>
2023-09-06 16:37:51 +0200

    Aggiunto back button nella sezione amministratore

--- CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/MainActivityAmministratore.kt
+++ CroceVerdePlus/app/src/main/java/com/example/croceverdeplus/MainActivityAmministratore.kt
@@ -1,16 +1,26 @@
 package com.example.croceverdeplus
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivityAmministratore : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activity_amministratore)
 
+        navController = findNavController(R.id.nav_host_fragment)
+
+        //imposto il back button
+        setupActionBarWithNavController(navController)
     }
+
+    //gestisce il comportamento del back button
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
